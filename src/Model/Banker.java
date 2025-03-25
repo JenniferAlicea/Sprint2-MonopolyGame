0f640e828f505733ceedc54e2f84a16d289e6a8b
Jennifer Alicea <aliceaj@moravian.edu>
2025-03-25 11:12:51 -0400

    Add empty Banker class

---
+++ src/Model/Banker.java
@@ -0,0 +1,47 @@
+package Model;
+
+public class Banker {
+    private TitleDeedCard[] titleDeedCards;
+    private int houses = 32;
+    private int hotels = 12;
+
+    public void auctionProperty(BoardSquare property) {
+
+    }
+
+    public void sellProperty(BoardSquare property, Player player) {
+
+    }
+
+    public void paySalary(Player player) {
+
+    }
+
+    public void sellHouse(Player player) {
+
+    }
+
+    public void sellHotel(Player player) {
+
+    }
+
+    public void loanMoney(Player player, int amount) {
+
+    }
+
+    public void collectTaxes(Player player) {
+
+    }
+
+    public void collectFines(Player player) {
+
+    }
+
+    public void collectLoans(Player player) {
+
+    }
+
+    public void collectInterest(Player player) {
+
+    }
+}
