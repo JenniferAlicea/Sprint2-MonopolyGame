08d8edd17dde6086feba73b9112c2e10ac22bd50
Anthony Dayoub <dayouba@moravian.edu>
2025-03-25 18:30:50 -0400

    New enums added TurnPhase and PlayerAction for turn feature

---
+++ src/Model/PlayerAction.java
@@ -0,0 +1,12 @@
+package Model;
+
+public enum PlayerAction {
+    BUY_PROPERTY,
+    AUCTION_PROPERTY,
+    BUILD_HOUSE,
+    BUILD_HOTEL,
+    MORTGAGE_PROPERTY,
+    UNMORTGAGE_PROPERTY,
+    TRADE,
+    END_TURN
+}
---
+++ src/Model/TurnPhase.java
@@ -0,0 +1,9 @@
+package Model;
+
+public enum TurnPhase {
+        ROLL_DICE,
+        MOVE_TOKEN,
+        HANDLE_SQUARE_ACTION,
+        PLAYER_ACTIONS,
+        END_TURN
+    }
